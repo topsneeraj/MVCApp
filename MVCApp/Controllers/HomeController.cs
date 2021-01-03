b3b5f5ccf5f6faf53ef96ae2cf4ecd188009d7e8
topsneeraj <agnihotrineeraj00@gmail.com>
2021-01-03 16:34:14 +0530

    Update HomeController.cs
    added featus 2 to main barach

--- MVCApp/Controllers/HomeController.cs
+++ MVCApp/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace MVCApp.Controllers
           /// code is edited is testying 
           /// this is agian new code added
           /// added new test controller 
+          // added featus 2
             return View();
         }
         public int  add(int a ,int b)
